fee6dde2d0ab0cacb61b6fbb81a1f3b938152f11
kyotometus <kyotometus@pm.me>
2024-10-02 11:37:55 +0100

    Implemented minimal GUI as a base

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿using Gtk;
+using Gdk;
+using System;
+
+class SimpleWindow : Gtk.Window
+{
+    public SimpleWindow() : base("Simple GtkSharp Window")
+    {
+        SetDefaultSize(400, 200);
+        SetPosition(WindowPosition.Center);
+        DeleteEvent += delegate { Application.Quit(); };
+
+        Box vbox = new Box(Orientation.Vertical, 5);
+
+        Pixbuf pixbuf = new Pixbuf("Assets/cat.jpg");
+        Pixbuf scaledPixbuf = pixbuf.ScaleSimple(400, 200, InterpType.Bilinear);
+
+        Image image = new Image(scaledPixbuf);
+        vbox.PackStart(image, false, false, 0);
+
+        Entry input = new Entry();
+        vbox.PackStart(input, false, false, 0);
+
+        Box hbox = new Box(Orientation.Horizontal, 5);
+
+        Button buttonSubmit = new Button("Submit");
+        buttonSubmit.Clicked += delegate
+        {
+            Console.WriteLine("Button clicked");
+            Console.WriteLine("Input: " + input.Text);
+
+            if (input.Text == "cat")
+            {
+                MessageDialog md = new MessageDialog(this, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "Correct, it's a cat!");
+                md.Run();
+                md.Destroy();
+            }
+            else
+            {
+                MessageDialog md = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Ok, "Wrong!!!!!");
+                md.Run();
+                md.Destroy();
+            }
+        };
+
+        Button buttonRegenerate = new Button("Regenerate");
+        buttonRegenerate.Clicked += delegate
+        {
+            Console.WriteLine("I'm afraid nothing happens here.");
+        };
+
+        hbox.PackStart(buttonSubmit, true, true, 0);
+        hbox.PackStart(buttonRegenerate, true, true, 0);
+
+        vbox.PackStart(hbox, false, false, 0);
+
+        Add(vbox);
+
+        ShowAll();
+    }
+
+    static void Main()
+    {
+        Application.Init();
+        new SimpleWindow();
+        Application.Run();
+    }
+}

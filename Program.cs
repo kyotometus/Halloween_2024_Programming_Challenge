605ed37e772cbd000c423ed20e3942465dd296f1
kyotometus <kyotometus@pm.me>
2024-10-03 14:16:03 +0100

    memory management and some minor fixes

--- Program.cs
+++ Program.cs
@@ -19,20 +19,16 @@ class SimpleWindow : Gtk.Window
         Resizable = false;
         DeleteEvent += delegate { Application.Quit(); };
 
-        // This function takes care of generating the captcha image and overlaying it on the window
-        generatedCaptchaText = phoneWave.GenerateRandomCaptchaWithOverlay("Assets/captcha.png", 500, 200);
+        // Generate captcha image in memory and overlay it on the window
+        var result = phoneWave.GenerateRandomCaptchaWithOverlay(500, 200);
+        generatedCaptchaText = result.captchaText;
+        overlay = new Image(result.captchaPixbuf);
 
         Fixed fixedContainer = new Fixed();
-
-        Pixbuf pixbufCaptcha = new Pixbuf("Assets/captcha.png");
-        Pixbuf scaledPixbufCaptcha = pixbufCaptcha.ScaleSimple(400, 200, InterpType.Bilinear);
-
-        overlay = new Image(scaledPixbufCaptcha);
-
         fixedContainer.Put(overlay, 0, 0);
 
         Entry input = new Entry();
-        input.WidthRequest = 400;
+        input.WidthRequest = 500;
         fixedContainer.Put(input, 0, 220);
 
         Box hbox = new Box(Orientation.Horizontal, 5);
@@ -40,57 +36,50 @@ class SimpleWindow : Gtk.Window
         // This is the ultra complex security system
         // Literally impossible to crack
         Button buttonSubmit = new Button("Submit");
-        buttonSubmit.WidthRequest = 190;
+        buttonSubmit.WidthRequest = 250;
         buttonSubmit.Clicked += delegate
         {
             Console.WriteLine("Button clicked");
             Console.WriteLine("Input: " + input.Text);
 
-            if (input.Text == generatedCaptchaText)
+            using (var md = new MessageDialog(this, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "You now have access to LaurieWired Youtube channel!"))
             {
-                MessageDialog md = new MessageDialog(this, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "You now have access to LaurieWired Youtube channel!");
+                if (input.Text == generatedCaptchaText)
+                {
+                    md.Text = "You now have access to LaurieWired Youtube channel!";
+                }
+                else if (input.Text == "LaurieWired" || input.Text == "lauriewired")
+                {
+                    md.Text = "Weird flex but ok";
+                }
+                else
+                {
+                    md.MessageType = MessageType.Error;
+                    md.Text = "Get some glasses!!!";
+                }
                 md.Run();
-                md.Destroy();
-            }
-            else if (input.Text == "LaurieWired" || input.Text == "lauriewired")
-            {
-                MessageDialog md = new MessageDialog(this, DialogFlags.Modal, MessageType.Info, ButtonsType.Ok, "Weird flex but ok");
-                md.Run();
-                md.Destroy();
-            }
-            else
-            {
-                MessageDialog md = new MessageDialog(this, DialogFlags.Modal, MessageType.Error, ButtonsType.Ok, "Get some glasses!!!");
-                md.Run();
-                md.Destroy();
             }
         };
 
         // In case the user has some skill issues
         Button buttonRegenerate = new Button("Regenerate");
-        buttonRegenerate.WidthRequest = 190;
+        buttonRegenerate.WidthRequest = 250;
         buttonRegenerate.Clicked += delegate
         {
-            generatedCaptchaText = phoneWave.GenerateRandomCaptchaWithOverlay("Assets/captcha.png", 500, 200);
-
-            Pixbuf newPixbufCaptcha = new Pixbuf("Assets/captcha.png");
-            Pixbuf newScaledPixbufCaptcha = newPixbufCaptcha.ScaleSimple(400, 200, InterpType.Bilinear);
-
-            overlay.Pixbuf = newScaledPixbufCaptcha;
+            var regenerateResult = phoneWave.GenerateRandomCaptchaWithOverlay(500, 200);
+            generatedCaptchaText = regenerateResult.captchaText;
+            overlay.Pixbuf = regenerateResult.captchaPixbuf;
 
             input.Text = "";
         };
-
 
         hbox.PackStart(buttonSubmit, false, false, 0);
         hbox.PackStart(buttonRegenerate, false, false, 0);
 
         hbox.WidthRequest = 400;
-
         fixedContainer.Put(hbox, 0, 260);
 
         Add(fixedContainer);
-
         ShowAll();
     }
 
